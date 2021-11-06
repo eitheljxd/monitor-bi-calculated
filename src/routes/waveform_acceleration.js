cfed21958790e0b2a0e8b8242cfafa4f718a91d4
EithelJ <ejmpradap@gmail.com>
2021-11-05 19:06:05 -0500

    set values

--- src/routes/waveform_acceleration.js
+++ src/routes/waveform_acceleration.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { waveform, surveys } = require("../config/db");
-const { fixNormalize,calcFft } = require("../utils/utils.math");
+const { fixNormalize, calcFft } = require("../utils/utils.math");
 
+let array = [];
 const routes = express.Router({
   mergeParams: true,
 });
@@ -9,19 +10,31 @@ const routes = express.Router({
 routes.get("/", async (req, res) => {
   let result = await getWaveforms();
   for (let i = 0; i < result.length; i++) {
-    //fixNormalize(result[i].dataValues.waveforms_accelerations);
-    calcFft(result[i].dataValues.waveforms_accelerations);
-      break;
+    let data = {
+      survey: result[i].dataValues.row_survey,
+      data: {
+        displacement: {},
+        acceleration: {},
+        velocity: {},
+        bands: {},
+      },
+    };
+    fixNormalize(result[i].dataValues.waveforms_accelerations);
+    data.acceleration = await calcFft(
+      result[i].dataValues.waveforms_accelerations
+    );
+    array.push(data);
   }
   res.json({
     success: true,
+    data: array,
     rows_affected: result.length,
   });
 });
 async function getWaveforms() {
   return await surveys.findAll({
-    limit: 1,
-    where: { state: 1,  row_survey: 17584  },
+    limit: 10,
+    where: { state: 1 },
     include: [waveform],
   });
 }
--- src/utils/utils.math.js
+++ src/utils/utils.math.js
@@ -1,89 +1,76 @@
 const { createModel } = require("polynomial-regression");
-const {
- evaluate
-} = require("mathjs");
-const fs = require('fs');
-var fft = require('fft-js').fft,
-    fftUtil = require('fft-js').util,
-    signal = [1,0,1,0];
+const { evaluate } = require("mathjs");
+const fs = require("fs");
+var fft = require("fft-js").fft,
+  fftUtil = require("fft-js").util,
+  signal = [1, 0, 1, 0];
 
 const fixNormalize = async (curve) => {
   let data = [];
   let dataPurge = [];
-  let data128 = []
+  let data128 = [];
   curve.map((a) => {
-
-      if(a.row_point == 707){
-        data.push([Number(a.dataValues.wfa_measure_x), Number(a.dataValues.wfa_measure_Y)]);
-      }
+    if (a.row_point == 707) {
+      data.push([
+        Number(a.dataValues.wfa_measure_x),
+        Number(a.dataValues.wfa_measure_Y),
+      ]);
+    }
   });
-
 
   ///
 
-   for(let index = 0; index < 128; index++) {
-      data128.push(data[index]);
-    
-    }
+  for (let index = 0; index < 128; index++) {
+    data128.push(data[index]);
+  }
 
-    data = data128;
-    console.log(data);
+  data = data128;
+  console.log(data);
   ///
   const model = createModel();
   model.fit(data, [3, 100]);
   let assertModel = model.expressions();
-  assertModel = assertModel['100'];
-  console.log("assertModel",assertModel);
-  for(let i = 0 ; i< data.length; i++){
+  assertModel = assertModel["100"];
+  console.log("assertModel", assertModel);
+  for (let i = 0; i < data.length; i++) {
     let replace = assertModel.replace(/\x/g, i);
-    let y =  evaluate(replace);
+    let y = evaluate(replace);
     dataPurge.push({
       x: curve[i].dataValues.wfa_measure_x,
-      y: y
-    })
+      y: y,
+    });
   }
 
-  fs.writeFileSync('./expressionsForGraphss2.json', JSON.stringify(dataPurge, null, 2));
+  fs.writeFileSync(
+    "./expressionsForGraphss2.json",
+    JSON.stringify(dataPurge, null, 2)
+  );
 };
-
-
 
 const calcFft = async (curve) => {
-
-
   let data = [];
-  let dataTest =[];
+  let dataTest = [];
   curve.map((a) => {
-      if(a.row_point == 709){
-        data.push(Number(a.dataValues.wfa_measure_Y));
-      }
+    data.push(Number(a.dataValues.wfa_measure_Y));
   });
 
+  console.log("data", data);
 
+  var phasors = fft(data);
+  const magnitudes = fftUtil.fftMag(phasors);
 
-  var phasors= fft(data);
-  const magnitudes = fftUtil.fftMag(phasors); 
-
-  for(let i = 0 ; i< magnitudes.length; i++){
+  for (let i = 0; i < magnitudes.length; i++) {
     dataTest.push({
       x: curve[i].dataValues.wfa_measure_x,
-      y: magnitudes[i]
-    })
+      y: magnitudes[i],
+    });
   }
-  console.log("dataTest",dataTest);
-  fs.writeFileSync('./expressionsForGraphss2.json', JSON.stringify(dataTest, null, 2));
-
- 
+  return dataTest;
 };
-
-
-
-
-
 
 // Or
 
 module.exports = {
   fixNormalize,
-  calcFft
+  calcFft,
 };
