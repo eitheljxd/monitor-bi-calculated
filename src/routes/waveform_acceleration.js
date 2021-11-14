aad4923f9a50bde785b435b7596b2cbe91e976f7
EithelJ <ejmpradap@gmail.com>
2021-11-13 19:42:05 -0500

    add displacement full ver

--- src/routes/waveform_acceleration.js
+++ src/routes/waveform_acceleration.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const { waveform, surveys } = require("../config/db");
-const { fixNormalize, calcFft } = require("../utils/utils.math");
+const {
+  fixNormalize,
+  calcFft,
+  integrateAcceleration,
+  integrateVelocity,
+} = require("../utils/utils.math");
+const fs = require("fs");
 
 let array = [];
 const routes = express.Router({
@@ -9,32 +15,30 @@ const routes = express.Router({
 
 routes.get("/", async (req, res) => {
   let result = await getWaveforms();
+  console.log(result.length);
   for (let i = 0; i < result.length; i++) {
     let data = {
       survey: result[i].dataValues.row_survey,
       data: {
-        displacement: {},
-        acceleration: {},
-        velocity: {},
+        displacement: [],
+        acceleration: [],
+        velocity: [],
         bands: {},
       },
     };
-    fixNormalize(result[i].dataValues.waveforms_accelerations);
-    data.acceleration = await calcFft(
+    data.data.acceleration = await calcFft(
       result[i].dataValues.waveforms_accelerations
     );
+    data.data.velocity = await integrateAcceleration(data.data.acceleration);
+    data.data.displacement = await integrateVelocity(data.data.velocity);
     array.push(data);
   }
-  res.json({
-    success: true,
-    data: array,
-    rows_affected: result.length,
-  });
+  res.json(array);
 });
 async function getWaveforms() {
   return await surveys.findAll({
     limit: 10,
-    where: { state: 1 },
+    where: { state: 1, row_survey: 18404 },
     include: [waveform],
   });
 }
--- src/utils/utils.math.js
+++ src/utils/utils.math.js
@@ -48,29 +48,126 @@ const fixNormalize = async (curve) => {
 };
 
 const calcFft = async (curve) => {
-  let data = [];
-  let dataTest = [];
-  curve.map((a) => {
-    data.push(Number(a.dataValues.wfa_measure_Y));
-  });
+  let consolid = [];
 
-  console.log("data", data);
-
-  var phasors = fft(data);
-  const magnitudes = fftUtil.fftMag(phasors);
-
-  for (let i = 0; i < magnitudes.length; i++) {
-    dataTest.push({
-      x: curve[i].dataValues.wfa_measure_x,
-      y: magnitudes[i],
-    });
+  // curve.map((a) => {
+  //   console.log("sd", a);
+  //   data.push(Number(a.dataValues.wfa_measure_Y));
+  //   return;
+  // });
+  let points = [];
+  //Extract the points
+  for (let index = 0; index < curve.length; index++) {
+    var ob = points.findIndex((x) => x == curve[index].dataValues.row_point);
+    ob === -1 ? points.push(curve[index].dataValues.row_point) : null;
   }
-  return dataTest;
+  // Search data for each points
+  for (let index = 0; index < points.length; index++) {
+    let data = [];
+    let dataY = [];
+    let dataX = [];
+    let result = {
+      row_point: null,
+      data: [],
+    };
+
+    let row_point = points[index];
+
+    curve.find((o) => {
+      if (o.dataValues.row_point === points[index]) {
+        dataY.push(Number(o.dataValues.wfa_measure_Y));
+        dataX.push(Number(o.dataValues.wfa_measure_x));
+      }
+    });
+    console.log("row_point", row_point);
+
+    var phasors = fft(dataY);
+    console.log("phasors", phasors.length);
+
+    const magnitudes = fftUtil.fftMag(phasors);
+    console.log("magnitudes", magnitudes.length);
+
+    for (let i = 0; i < magnitudes.length; i++) {
+      data.push({
+        x: dataX[i],
+        y: magnitudes[i],
+      });
+    }
+    result.row_point = row_point;
+    result.data = data;
+    console.log("data ", result.data.length);
+    consolid.push(result);
+  }
+
+  return consolid;
 };
 
+const integrateAcceleration = async (curve) => {
+  let consolid = [];
+  const FIXED_MULTIPLIER_VS = 670.0;
+  curve.map((row) => {
+    const row_point = row.row_point;
+    let result = {
+      row_point: row_point,
+      data: [],
+    };
+    let dataAux = [];
+    const data = row.data;
+    data.map((object, i) => {
+      dataAux.push({
+        x: object.x,
+        y: object.y / (FIXED_MULTIPLIER_VS * (i + 1)),
+      });
+    });
+    result.data = dataAux;
+    consolid.push(result);
+  });
+  // for (let i = 0; i < curve.length; i++) {
+  //     const row_point = curve[i].row_point;
+  //     let result = {
+  //       row_point: row_point,
+  //       data : []
+  //     };
+  //     let dataAux = [];
+  //     const data = curve[i].data;
+  //     data.forEach(function (object, i) {
+  //       dataAux.push({
+  //         x: object.x,
+  //         y: (object.y / (FIXED_MULTIPLIER_VS * (i+ 1))),
+  //       });
+  //     });
+  //     result.data = dataAux;
+  //     consolid.push(result);
+  // }
+  return consolid;
+};
+
+const integrateVelocity = async (curve) => {
+  let consolid = [];
+  const FIXED_MULTIPLIER_VS = 670.0;
+  curve.map((row) => {
+    const row_point = row.row_point;
+    let result = {
+      row_point: row_point,
+      data: [],
+    };
+    let dataAux = [];
+    const data = row.data;
+    data.map((object, i) => {
+      dataAux.push({
+        x: object.x,
+        y: FIXED_MULTIPLIER_VS * (object.y / ((2 * Math.PI) * (i + 1)),
+      });
+    });
+    result.data = dataAux;
+    consolid.push(result);
+  });
+};
 // Or
 
 module.exports = {
   fixNormalize,
   calcFft,
+  integrateAcceleration,
+  integrateVelocity,
 };
